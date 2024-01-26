d8b2aef5fe47692d3b0b14ba1f0c798cd9a8e938
Pranav Kokate <pranavkokate14@gmail.com>
2024-01-26 20:55:56 +0530

    class based components

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,14 @@
-const User  = (props) => {
+import { useState } from "react";
+
+const User  = ({ name }) => {
+
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(1);
+
   return<div className="user-card">
-    <h2>Name: {props.name}</h2>
+    <h1>Count: {count}</h1>
+    <h1>Count: {count2}</h1>
+    <h2>Name: {name}</h2>
     <h2>Location: Aurangabad</h2>
     <h2>Contact: @pranav</h2>
   </div>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,16 +5,26 @@ class UserClass extends React.Component{
   constructor(props){
     // to inherit the properties of parent class and access the this keyword inside the class
     super(props);
-
+    this.state = {
+      count: 0,
+    };
     console.log(props);
   }
 
   // render is used to display this jsx into ui
   render() {
 
+    //destructuring
     const { name, location } = this.props;
+    const { count, count2 } = this.state;
 
     return<div className="user-card">
+    <h1>Count: {count}</h1>  
+    <button onClick={() => {
+      this.setState({
+        count : this.state.count + 1
+      })
+    }}>Count Increase</button>
     <h2>Name: {name}</h2>
     <h2>Location: {location}</h2>
     <h2>Contact: @pranav</h2>
